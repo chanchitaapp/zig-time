ccf158f7cfd4f63e435123cfa11f4de359e2749b
Luis Quiñones <lpaandres2020@gmail.com>
2024-10-05 18:21:58 -0600

    feat(build): specifying `pub const pkg` with `std.build.Pkg`

--- build.zig
+++ build.zig
@@ -1,16 +1,40 @@
 const std = @import("std");
 
+pub const pkg = std.build.Pkg{
+    .name = "time",
+    .path = .{ .source = thisDir() ++ "/src/main.zig" },
+};
+
 pub fn build(b: *std.Build) void {
     const t = b.addTest(.{
         .root_source_file = b.path("main.zig"),
     });
 
-    const extras = b.dependency("extras", .{});
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const extras = b.dependency("extras", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
     t.root_module.addImport("extras", extras.module("extras"));
+
+    const lib = b.addStaticLibrary(.{
+        .name = "time",
+        .root_source_file = b.path("time.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(lib);
 
     const run_t = b.addRunArtifact(t);
 
     const t_step = b.step("test", "Run all the tests.");
     t_step.dependOn(&run_t.step);
+}
+
+fn thisDir() []const u8 {
+    return std.fs.path.dirname(@src().file) orelse ".";
 }
